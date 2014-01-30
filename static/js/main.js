28015e81dd4657701a1188f13deb91c17248b7d1
Steve Vadocz <uplneinak@gmail.com>
2014-01-30 19:23:51 +0000

    JS setup. Added requirejs and toggleMenu module.

---
+++ static/js/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+require.config({
+    "baseUrl": "static/js",
+    "paths": {
+        //jquery: '//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min'
+        jquery: 'lib/jquery/jquery'
+    }
+});
+
+require([
+    'jquery',
+    'modules/toggleMenu'
+], function ($, ToggleMenu) {
+    
+    var toggleMenu = new ToggleMenu();
+        toggleMenu.init();
+});
---
+++ static/js/modules/toggleMenu.js
@@ -0,0 +1,41 @@
+/**
+ * Toogle menu module.
+ */
+define([
+    'jquery'
+], function($) {
+    'use strict';
+
+    var ToggleMenu = function() {
+
+        var self = null;
+
+        return {
+            // DOM elements
+            el: {
+                $mainHolder: $('.main'),
+                $navlink: $('[data-module="tooglemenu"]'),
+                $sidebar: $('.main__container')
+            },
+
+            init: function() {
+
+                self = this;
+
+                self.bindEvents();
+            },
+
+            bindEvents: function() {
+                self.el.$navlink.on('click', function(event) {
+                    event.preventDefault();
+
+                    $(this).toggleClass('is-active');
+                    self.el.$mainHolder.toggleClass('is-active');
+                    self.el.$sidebar.toggleClass('is-active');
+                });
+            }
+        };
+    };
+
+    return ToggleMenu;
+});
