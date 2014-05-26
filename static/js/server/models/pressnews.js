4ba03c0ed24cdfc3ce749f6d863a7f90d59e2eba
Steve Vadocz <uplneinak@gmail.com>
2014-05-26 12:50:38 +0100

    Partners grid on mobile fix. Grammar fixes.

---
+++ static/js/server/models/pressnews.js
@@ -0,0 +1,21 @@
+/**
+* Module dependencies.
+*/
+
+var mongoose = require('mongoose'),
+    Schema   = mongoose.Schema;
+
+/**
+ * Program schema
+ */
+var PressnewsSchema = new Schema({
+    title:  {type: String, default: ''},
+    text:    {type: String, default: ''},
+    img:     {type: String, default: ''},
+    path:    {type: String, default: ''}
+},
+{
+    collection: 'pressnews'
+});
+
+mongoose.model('Pressnews', PressnewsSchema, 'pressnews');
